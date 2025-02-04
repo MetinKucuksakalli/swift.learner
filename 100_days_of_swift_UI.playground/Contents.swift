2e1468e1c575c5da2bc281e983244845cde03210
MK <kucuksakalli.m@gmail.com>
2025-02-04 22:54:47 +1100

    day 3

--- 100_days_of_swift_UI.playground/Contents.swift
+++ 100_days_of_swift_UI.playground/Contents.swift
@@ -104,4 +104,98 @@ print(message)
 let number2 = 11
 let missionMessage = "Apollo " + String(number2) + " mission is a success!"
 
-t
+// Day 3
+var beatles = ["John", "Paul", "George", "Ringo"]
+let numbers = [4, 8, 15, 16, 23, 42]
+var temperatures = [25.3, 28.2, 26.4]
+
+print(beatles[0])
+print(numbers[1])
+
+beatles.append("Adrian")
+
+let firstBeatle = beatles[0]
+let firstNumber = numbers[0]
+
+var albums = Array<String>()
+albums.append("Folklore")
+albums.append("Fearless")
+albums.append("Red")
+
+print(albums.count)
+
+var characters = ["Lana", "Pam", "Ray", "Sterling"]
+
+print(characters.count)
+
+characters.remove(at: 2)
+print(characters)
+
+characters.removeAll()
+print(characters.count)
+
+
+let bondMovies = ["Casino Royale", "Spectre", "No Time To Die"]
+print(bondMovies.contains("Frozen"))
+
+let cities = ["London", "Tokyo", "Rome", "Budapest"]
+print(cities.sorted())
+
+let presidents = ["Bush", "Obama", "Trump", "Biden"]
+let reversedPresidents = presidents.reversed()
+print(reversedPresidents)
+
+var employee = ["Taylor Swift", "Singer", "Nashville"]
+
+print("Name: \(employee[0])")
+print("Job title: \(employee[1])")
+print("Location: \(employee[2])")
+
+let employee2 = [
+    "name": "Taylor Swift",
+    "job": "Singer",
+    "location": "Nashville"
+]
+
+print(employee2["name", default:"Unkown"])
+
+let hasGraduated = [
+    "Eric": false,
+    "Maeve": true,
+    "Otis": false
+]
+
+let olympics = [
+    2012: "London",
+    2016: "Rio de Janeiro",
+    2021: "Tokyo"
+]
+
+print(olympics[2012, default: "Unknown"])
+var heights = [String: Int]()
+heights["Yao Ming"] = 229
+heights["Shaquille O'Neal"] = 216
+heights["LeBron James"] = 206
+
+let people1 = Set(["Denzel Washington", "Tom Cruise", "Nicolas Cage", "Samuel L Jackson"])
+print(people1)
+
+var people = Set<String>()
+people.insert("Denzel Washington")
+people.insert("Tom Cruise")
+people.insert("Nicolas Cage")
+people.insert("Samuel L Jackson")
+print(people)
+
+var selected = "Monday"
+selected = "Tuesday"
+selected = "January"
+selected = "Friday "
+
+enum Weekday {
+    case monday, tuesday, wednesday, thursday, friday
+}
+
+var day = Weekday.monday
+day = Weekday.tuesday
+day = Weekday.friday
