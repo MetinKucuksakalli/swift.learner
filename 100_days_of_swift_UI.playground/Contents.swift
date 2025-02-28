e4b3794f45308c0c1b86515d828aced8aef7f636
MK <kucuksakalli.m@gmail.com>
2025-02-28 21:16:31 +1100

    day 6

--- 100_days_of_swift_UI.playground/Contents.swift
+++ 100_days_of_swift_UI.playground/Contents.swift
@@ -396,3 +396,110 @@ let theme = Theme.dark
 
 let background = theme == .dark ? "black" : "white"
 print(background)
+
+///day 6
+///
+
+let platforms = ["iOS", "macOS", "tvOS", "watchOS"]
+
+for os in platforms {
+    print("Swift work grat on \(os).")
+}
+
+for i in 1...12 {
+    print("5x \(i) is \(5*i)")
+}
+
+for i in 1...12 {
+    print("5x \(i) is \(5*i)")
+}
+
+for i in 1...12 {
+    print("The \(i) times table:")
+
+    for j in 1...12 {
+        print("  \(j) x \(i) is \(j * i)")
+    }
+
+    print()
+}
+
+for i in 1...5 {
+    print("Counting from 1 through 5: \(i)")
+}
+
+print()
+
+for i in 1..<5 {
+    print("Counting 1 up to 5: \(i)")
+}
+
+var lyric = "Haters gonna"
+
+for _ in 1...5 {
+    lyric += " hate"
+}
+
+print(lyric)
+
+var countdown = 10
+
+while countdown > 0 {
+    print("\(countdown)...")
+    countdown -= 1
+}
+
+print("Blast off")
+
+var roll = 0
+while roll != 20 {
+    roll = Int.random(in:1...100)
+    print("I rolled a \(roll)")
+
+}
+
+print("Critical hit!")
+
+
+let filenames = ["me.jpg", "work.txt", "sophie.jpg", "logo.psd"]
+
+for filename in filenames {
+    if filename.hasSuffix(".jpg") == false {
+        continue
+    }
+
+    print("Found picture: \(filename)")
+}
+
+
+let number1 = 4
+let number3 = 14
+var multiples = [Int]()
+
+for i in 1...100_000 {
+    if i.isMultiple(of: number1) && i.isMultiple(of: number2) {
+        multiples.append(i)
+
+        if multiples.count == 10 {
+            break
+        }
+    }
+}
+
+print(multiples)
+
+for i in 1...100 {
+    if i.isMultiple(of: 3) && i.isMultiple(of: 5) {
+        print("FizzBuzz")
+    }
+        else if i.isMultiple(of: 5) {
+            print("Buzz")
+        }
+        else if i.isMultiple(of: 3) {
+            print("Fizz")
+        }
+    else {
+        print(i)
+    }
+}
+
